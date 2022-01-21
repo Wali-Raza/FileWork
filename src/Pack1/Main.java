8fbe7edb4e9257d6af92b4c370b6a45c20341827
Wali Raza <walimanzoor321@gmail.com>
2022-01-21 14:58:11 +0500

    Creating File,writing into the FIle and then Reading it using While :)

--- src/Pack1/Main.java
+++ src/Pack1/Main.java
@@ -1,46 +1,29 @@
 package Pack1;
-
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-	// write your code here
-//        System.out.println("Wali Raza here");
-//        File f=new File("abc.txt"); //this will not creat phiscal text file i
-//        //itll creat a virtual file
-//        System.out.println(f.exists());
-//        f.createNewFile();
-//        //NOew file will be created physically
-//        System.out.println(f.exists());
-//        File f1=new File("E:\\FileWork");
-//        System.out.println(f1.exists());
-//        File f=new File("Wali1");
-//        f.mkdir();
-//        String[] s=f.list();
-//        System.out.println(f.length());
-//        System.out.println(s);
-//        System.out.println((f.mkdir()));
-//        //System.out.println(f.exists());
-//        File f1=new File("Wali1,Wali2.txt");
-//        f.createNewFile();
-//        //System.out.println(f.exists());
 
-        int count=0;
-        File f=new File("E:\\FileWork");
-        String[] s=f.list();
-        //long l=f.length();
-        //System.out.println("Length="+l);
-        for(String s1:s)
+    public static void main(String[] args) throws IOException
+    {
+        File f=new File("abc2.txt");
+        FileWriter  fw=new FileWriter(f);
+        fw.write("walimanzoor321@gmail.com");
+        fw.write("\nalikkcs42@gmail.com");
+        fw.flush();
+        fw.close();
+        FileReader fr=new FileReader(f);
+        //char[] ch=new char[(int)f.length()];
+        System.out.println(f.length());
+        int  i=fr.read();
+        while (i!=-1)
         {
-
-            System.out.println(s1);
-            count=count+1;
-
+            System.out.print((char) i);
+            i=fr.read();
         }
-
-        System.out.println("TOtal number of files="+count);
-        
+        fr.close();
     }
 }
